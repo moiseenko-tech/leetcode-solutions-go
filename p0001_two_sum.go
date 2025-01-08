1b03e237fd1fe2877e26fe2abc0e0463ef829616
Andrei Moiseenko <moiseenko.tech@gmail.com>
2025-01-08 15:15:29 +0200

    Add solution for #1 Two Sum.

---
+++ p0001_two_sum.go
@@ -0,0 +1,33 @@
+package p0001_two_sum
+
+import "sort"
+
+func twoSum(nums []int, target int) []int {
+	N := len(nums)
+
+	// positions in the original slice
+	indexes := make([]int, N)
+	for i := 0; i < N; i++ {
+		indexes[i] = i
+	}
+
+	// sort nums indexes
+	sort.Slice(indexes, func(i, j int) bool {
+		return nums[indexes[i]] < nums[indexes[j]]
+	})
+
+	// perform a search in sorted slice
+	i, j := 0, N-1
+	for i < j {
+		sum := nums[indexes[i]] + nums[indexes[j]]
+		if sum == target {
+			break
+		} else if sum > target {
+			j--
+		} else {
+			i++
+		}
+	}
+
+	return []int{indexes[i], indexes[j]}
+}
